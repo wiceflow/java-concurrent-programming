ee4d7c531218e94823c038bb77ceedd4cee98643
wiceflow <183173916@qq.com>
2018-04-04 17:43:58 +0800

    demo015

---
+++ src/main/java/com/wiceflow/example004/demo015/Task.java
@@ -0,0 +1,23 @@
+package com.wiceflow.example004.demo015;
+
+/**
+ * @author Iceflow
+ */
+public class Task {
+
+	private int id;
+	private int price ;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	} 
+	
+}
